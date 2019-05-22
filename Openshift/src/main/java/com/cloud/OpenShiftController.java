c000247f829ffbde78e81b0e0ed98699afbcc6b0
ChandranSrinivasan <ChandranSrinivasan@DESKTOP-Q12O97K.in.ibm.com>
2019-05-22 19:34:12 +0530

    removed unwanted import

--- Openshift/src/main/java/com/cloud/OpenShiftController.java
+++ Openshift/src/main/java/com/cloud/OpenShiftController.java
@@ -1,13 +1,5 @@
 package com.cloud;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
